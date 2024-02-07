623641bb084ca9fc1af8003a20bc5251f4b3b74d
Bearslay <barfunkel@protonmail.com>
2024-02-07 17:51:09 -0500

    Transition point interface work

--- include/sleep.hpp
+++ include/sleep.hpp
@@ -4,10 +4,9 @@
 #include "ncursespp.hpp"
 #include "timing.hpp"
 
-#define MAINMENU 0
-#define SETTINGS_MAIN 1
-#define SETTINGS_UPTIME 2
-#define SETTINGS_DOWNTIME 3
+#define TRANS_MAIN 0
+#define TRANS_BUFFER 1
+#define TRANS_WARNING 2
 
 namespace Sleep {
     /// @brief Convert an std::string to an std::wstring
@@ -133,52 +132,6 @@ namespace Sleep {
                 Win.wstr(Win.wstrp(Win.wstrp(15, 64, strtowstr(Timing::mtime.getWeekdayStr(false))), L": "), strtowstr(Downtime[Timing::mtime.getWeekdayNum()].getMainTime(true)));
             }
 
-            void rSettingsTransition(bool uptime) {
-                // Back
-                Win.dbox(4, Win.gdimx() - 14, 7, 9, {LIGHT_SOFT, DASHED_NONE}, NPP_RED);
-                Win.wmstr(6, Win.gdimx() - 11, "X", MTEXT_6x6, NPP_RED);
-
-                // Copy Time
-                Win.dbox(12, Win.gdimx() - 14, 7, 9, {LIGHT_SOFT, DASHED_NONE}, NPP_ORANGE);
-                Win.wmstr(14, Win.gdimx() - 11, "C", MTEXT_6x6, NPP_ORANGE);
-
-                // Paste Time
-                Win.dbox(20, Win.gdimx() - 14, 7, 9, {LIGHT_SOFT, DASHED_NONE}, NPP_ORANGE);
-                Win.wmstr(22, Win.gdimx() - 11, "P", MTEXT_6x6, NPP_ORANGE);
-
-                // Sync Days
-                Win.dbox(28, Win.gdimx() - 14, 7, 9, {LIGHT_SOFT, DASHED_NONE}, NPP_YELLOW);
-                Win.wmstr(30, Win.gdimx() - 11, "D", MTEXT_6x6, NPP_YELLOW);
-
-                // Separation Box
-                Win.dbox(6, 17, 29, 85);
-            }
-
-            int update(unsigned char state) {
-                Timing::mtime.update(true);
-
-                switch (state) {
-                    case MAINMENU:
-                        rMainMenu();
-                        break;
-                    case SETTINGS_MAIN:
-                        rSettingsMain();
-                        break;
-                    case SETTINGS_UPTIME:
-                        rSettingsTransition(true);
-                        break;
-                    case SETTINGS_DOWNTIME:
-                        rSettingsTransition(false);
-                        break;
-                }
-
-                if (Uptime[Timing::mtime.getWeekdayNum()].checkMain()) {
-
-                }
-
-                return 0;
-            }
-
             /// @brief Go into the settings menu
             /// @returns True to stay in the program, false to quit the program
             void settingsMenu() {
@@ -192,7 +145,8 @@ namespace Sleep {
 
                 int ch, state;
                 while (true) {
-                    state = update(SETTINGS_MAIN);
+                    rSettingsMain();
+                    state = update();
 
                     if ((ch = Win.gchar(false)) == KEY_MOUSE) {
                         if (npp::Mouse.gmouse(ch)) {
@@ -218,18 +172,91 @@ namespace Sleep {
                 }
             }
 
+            void rSettingsTransition(bool uptime, unsigned char transState, unsigned char day) {
+                // Time
+                Win.wmstr(2, 42 - (!Use24Hr ? 6 : 0), Timing::mtime.getTimeFormatted(Use24Hr), MTEXT_6x6);
+
+                // Back
+                Win.dbox(4, Win.gdimx() - 14, 7, 9, {LIGHT_SOFT, DASHED_NONE}, NPP_RED);
+                Win.wmstr(6, Win.gdimx() - 11, "X", MTEXT_6x6, NPP_RED);
+
+                // Copy Time
+                Win.dbox(12, Win.gdimx() - 14, 7, 9, {LIGHT_SOFT, DASHED_NONE}, NPP_ORANGE);
+                Win.wmstr(14, Win.gdimx() - 11, "C", MTEXT_6x6, NPP_ORANGE);
+
+                // Paste Time
+                Win.dbox(20, Win.gdimx() - 14, 7, 9, {LIGHT_SOFT, DASHED_NONE}, NPP_ORANGE);
+                Win.wmstr(22, Win.gdimx() - 11, "P", MTEXT_6x6, NPP_ORANGE);
+
+                // Sync Days
+                Win.dbox(28, Win.gdimx() - 14, 7, 9, {LIGHT_SOFT, DASHED_NONE}, NPP_YELLOW);
+                Win.wmstr(30, Win.gdimx() - 11, "D", MTEXT_6x6, NPP_YELLOW);
+
+                // Change Main Time
+                Win.dbox(4, 4, 7, 9, {LIGHT_SOFT, DASHED_NONE}, transState == TRANS_MAIN ? NPP_CYAN : NPP_TEAL);
+                Win.wmstr(6, 7, "M", MTEXT_6x6, transState == TRANS_MAIN ? NPP_CYAN : NPP_TEAL);
+
+                // Change Buffer Time
+                Win.dbox(12, 4, 7, 9, {LIGHT_SOFT, DASHED_NONE}, transState == TRANS_BUFFER ? NPP_CYAN : NPP_TEAL);
+                Win.wmstr(14, 7, "B", MTEXT_6x6, transState == TRANS_BUFFER ? NPP_CYAN : NPP_TEAL);
+
+                // Change Warning Time
+                Win.dbox(20, 4, 7, 9, {LIGHT_SOFT, DASHED_NONE}, transState == TRANS_WARNING ? NPP_CYAN : NPP_TEAL);
+                Win.wmstr(22, 7, "W", MTEXT_6x6, transState == TRANS_WARNING ? NPP_CYAN : NPP_TEAL);
+
+                // Sync Times
+                Win.dbox(28, 4, 7, 9, {LIGHT_SOFT, DASHED_NONE}, NPP_BLUE);
+                Win.wmstr(30, 7, "T", MTEXT_6x6, NPP_BLUE);
+
+                // Weekdays
+                for (unsigned char i = 0; i < 7; i++) {
+                    Win.dbox(36, 4 + i * 16, 7, 15, {LIGHT_SOFT, DASHED_NONE}, i == day ? NPP_LIGHT_GRAY : NPP_DARK_GRAY);
+                    Win.wmstr(38, 6 + i * 16, Timing::Keys.weekdayNamesAbb[i], MTEXT_6x6, i == day ? NPP_LIGHT_GRAY : NPP_DARK_GRAY);
+                }
+
+                // Separation Box
+                Win.dbox(6, 17, 29, 85);
+
+                // Titles
+                Win.wmstr(8, 22, uptime ? " CHANGE UPTIME " : "CHANGE DOWNTIME", MTEXT_8x8);
+                Win.wmstr(13, 27 + (transState == TRANS_BUFFER ? 3 : 0) + (transState == TRANS_MAIN ? 7 : 0), transState == TRANS_MAIN ? "- Main Time -" : (transState == TRANS_BUFFER ? "- Buffer Time -" : "- Warning Time -"), MTEXT_6x6);
+            
+                // Current Transition Time
+                Win.wmstr(23, 40, uptime ? (transState == TRANS_MAIN ? Uptime[day].getMainTime() : (transState == TRANS_BUFFER ? Uptime[day].getBufferTime() : Uptime[day].getWarningTime())) : (transState == TRANS_MAIN ? Downtime[day].getMainTime() : (transState == TRANS_BUFFER ? Downtime[day].getBufferTime() : Downtime[day].getWarningTime())), MTEXT_8x8);
+            }
+
             void settingsTransition(bool uptime) {
                 npp::Button back = npp::Button(Win.gposy() + 4, Win.gposx() + Win.gdimx() - 14, 7, 9, {M1_CLICK});
                 npp::Button copyTime = npp::Button(Win.gposy() + 12, Win.gposx() + Win.gdimx() - 14, 7, 9, {M1_CLICK});
                 npp::Button pasteTime = npp::Button(Win.gposy() + 20, Win.gposx() + Win.gdimx() - 14, 7, 9, {M1_CLICK});
                 npp::Button syncDays = npp::Button(Win.gposy() + 28, Win.gposx() + Win.gdimx() - 14, 7, 9, {M1_CLICK});
+                
+                npp::Button editMain = npp::Button(Win.gposy() + 4, Win.gposx() + 4, 7, 9, {M1_CLICK});
+                npp::Button editBuffer = npp::Button(Win.gposy() + 12, Win.gposx() + 4, 7, 9, {M1_CLICK});
+                npp::Button editWarning = npp::Button(Win.gposy() + 20, Win.gposx() + 4, 7, 9, {M1_CLICK});
+                npp::Button syncTimes = npp::Button(Win.gposy() + 28, Win.gposx() + 4, 7, 9, {M1_CLICK});
+                
+                npp::Button addHour = npp::Button(Win.gposy() + 17, Win.gposx() + 40, 5, 9, {M1_CLICK});
+                npp::Button addMinute = npp::Button(Win.gposy() + 17, Win.gposx() + 55, 5, 9, {M1_CLICK});
+                npp::Button addSecond = npp::Button(Win.gposy() + 17, Win.gposx() + 70, 5, 9, {M1_CLICK});
+                npp::Button removeHour = npp::Button(Win.gposy() + 28, Win.gposx() + 40, 5, 9, {M1_CLICK});
+                npp::Button removeMinute = npp::Button(Win.gposy() + 28, Win.gposx() + 55, 5, 9, {M1_CLICK});
+                npp::Button removeSecond = npp::Button(Win.gposy() + 28, Win.gposx() + 70, 5, 9, {M1_CLICK});
+
+                npp::Button toggle24Hr = npp::Button(Win.gposy() + 21, Win.gposx() + 21, 8, 13, {M1_CLICK});
+                npp::Button toggleAMPM = npp::Button(Win.gposy() + 21, Win.gposx() + 85, 8, 13, {M1_CLICK});
+
+                std::vector<npp::Button> weekday;
+                for (unsigned char i = 0; i < 7; i++) {weekday.emplace_back(npp::Button(Win.gposy() + Win.gdimy() - 10, Win.gposx() + 4 + i * 16, 7, 15, {M1_CLICK}));}
 
                 Win.reset();
                 Win.dbox();
 
                 int ch, state;
+                unsigned char transState = TRANS_MAIN, day = Timing::mtime.getWeekdayNum();
                 while (true) {
-                    state = update(2 + !uptime);
+                    rSettingsTransition(uptime, transState, day);
+                    state = update();
 
                     if ((ch = Win.gchar(false)) == KEY_MOUSE) {
                         if (npp::Mouse.gmouse(ch)) {
@@ -241,6 +268,16 @@ namespace Sleep {
                         if (ch == 'q') {return;}
                     }
                 }
+            }
+
+            int update() {
+                Timing::mtime.update(true);
+
+                if (Uptime[Timing::mtime.getWeekdayNum()].checkMain()) {
+
+                }
+
+                return 0;
             }
 
         public:
@@ -268,7 +305,8 @@ namespace Sleep {
 
                 int ch, state;
                 while (true) {
-                    state = update(MAINMENU);
+                    rMainMenu();
+                    state = update();
                     
                     if ((ch = Win.gchar(false)) == KEY_MOUSE) {
                         if (npp::Mouse.gmouse(ch)) {
